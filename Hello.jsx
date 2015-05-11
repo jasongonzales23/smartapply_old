d1ac587a0d224663af5c301fc827db9dcf588f19
Jason Gonzales <jason.gonzales23@gmail.com>
2015-05-10 18:27:08 -0700

    wip routing and auth

--- Hello.jsx
+++ Hello.jsx
@@ -1,8 +1,30 @@
 'use strict'
+var Firebase = require('firebase')
 var React = require('react')
+var firebaseRef = new Firebase('https://resplendent-heat-940.firebaseio.com/');
+
 module.exports = React.createClass({
-    displayName: 'HelloReact',
-    render: function(){
-        return <div>Hello React</div>
+  componentWillMount: function() {
+    var firebaseValue = "still nothing asshole";
+
+    firebaseRef.child("testKey").on("value", function(snapshot) {
+      var firebaseValue = snapshot.val();
+      this.setState({
+        thing: firebaseValue
+      });
+    }.bind(this));
+
+  },
+
+  getInitialState: function() {
+    var firebaseValue = "nothing yet";
+
+    return {
+      thing: firebaseValue
     }
+  },
+
+  render: function(){
+      return <div>Hello {this.state.thing}</div>
+  }
 });
--- index.jsx
+++ index.jsx
@@ -1,5 +1,68 @@
 'use strict'
-var React = require('react')
-var Hello = require('./Hello')
-React.render(<Hello />, document.getElementById('content'));
+var React = require('react');
+var Router = require('react-router');
+//var Hello = require('./Hello');
+var { Route, DefaultRoute, RouteHandler, Link } = Router;
 
+var requireAuth = (Component) => {
+  return class Authenticated extends React.Component {
+    static willTransitionTo(transition) {
+      if (!auth.loggedIn()) {
+        transition.redirect('/login', {}, {'nextPath' : transition.path});
+      }
+    }
+    render () {
+      return <Component {...this.props}/>
+    }
+  }
+};
+
+var auth = {
+  loggedIn: function() {
+    return false;
+  }
+};
+
+var Login = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h2>Login</h2>
+        <Link to="jobs">Jobs</Link>
+      </div>
+    );
+  }
+});
+
+var Jobs = requireAuth(class extends React.Component {
+  render () {
+    return (
+      <div>
+        <h2>Jobs</h2>
+        <Link to="login">Login</Link>
+      </div>
+    );
+  }
+});
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h1>App</h1>
+        <RouteHandler/>
+      </div>
+    );
+  }
+});
+
+var routes = (
+  <Route handler={App}>
+    <DefaultRoute name="jobs" handler={Jobs}/>
+    <Route name="login" handler={Login}/>
+  </Route>
+);
+
+Router.run(routes, function(Handler) {
+  React.render(<Handler/>, document.getElementById('content'));
+});
